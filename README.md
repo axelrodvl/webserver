b9b2086c37120837102f6c1db32a2b9bf74fcfba
Vadim Axelrod <vadim@axelrod.co>
2020-06-16 22:30:55 +0300

    Server version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # HTTP 1.1 Web Server
 
+Try it on [axelrod.co](http://axelrod.co:4450).
+
 Web server with the limited implementation of [RFC 2616](https://www.ietf.org/rfc/rfc2616.txt) (`Hypertext Transfer Protocol - HTTP/1.1`).
 
 Written in plain Java 11.
--- src/main/java/co/axelrod/webserver/impl/http/protocol/response/HttpResponse.java
+++ src/main/java/co/axelrod/webserver/impl/http/protocol/response/HttpResponse.java
@@ -9,8 +9,11 @@ import java.nio.charset.StandardCharsets;
 public class HttpResponse extends Response {
     private static final String CRLF = "\r\n";
 
-    private final String httpVersion = "HTTP/1.1";
+    private static final String HTTP_VERSION = "HTTP/1.1";
     private final HttpStatus httpStatus;
+
+    private static final String SERVER = "axelrod-webserver/0.0.1";
+
     private final byte[] body;
 
     public HttpResponse(HttpStatus httpStatus, byte[] body) {
@@ -20,12 +23,13 @@ public class HttpResponse extends Response {
 
     private byte[] getStatusLine() {
         return (
-                httpVersion + " " + httpStatus.getStatusCode() + " " + httpStatus.getReasonPhrase() + CRLF
+                HTTP_VERSION + " " + httpStatus.getStatusCode() + " " + httpStatus.getReasonPhrase() + CRLF
         ).getBytes(StandardCharsets.UTF_8);
     }
 
     public void writeResponse(OutputStream out) throws IOException {
         out.write(getStatusLine());
+        out.write(getHeader("Server", SERVER));
 
         if (body != null) {
             out.write(getContentLength());
@@ -35,8 +39,12 @@ public class HttpResponse extends Response {
     }
 
     private byte[] getContentLength() {
+        return getHeader("Content-Length", Integer.toString(body.length));
+    }
+
+    private byte[] getHeader(String name, String value) {
         return (
-                "Content-Length: " + body.length + CRLF
+                name + ": " + value + CRLF
         ).getBytes(StandardCharsets.UTF_8);
     }
 }
